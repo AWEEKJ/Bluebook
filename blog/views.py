e3db0f005557525a67adb80ed6e3307f3575867e
Hanju Jo <gkswn121@gmail.com>
2016-10-23 15:48:49 +0900

    Til Chapter7

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic.dates import DayArchiveView, TodayArchiveView
 
+from tagging.models import Tag, TaggedItem
+from tagging.views import TaggedObjectList
+
 from blog.models import Post
+
+
+class TagTV(TemplateView):
+    template_name = 'tagging/tagging_cloud.html'
 
 
 class PostLV(ListView):
@@ -10,6 +17,11 @@ class PostLV(ListView):
     template_name = 'blog/post_all.html'
     context_object_name = 'posts'
     paginate_by = 2
+
+
+class PostTOL(TaggedObjectList):
+    model = Post
+    template_name = 'tagging/tagging_post_list.html'
 
 
 class PostDV(DetailView):
